a1ddc12eb9f56f70644fa22a87579407a06f883a
cnc-lasercraft <96776729+cnc-lasercraft@users.noreply.github.com>
2026-02-07 10:26:33 +0100

    Add constants for water saver component

---
+++ custom_components/water_saver/const.py
@@ -0,0 +1,10 @@
+DOMAIN = "water_saver"
+
+CONF_TOPIC = "topic"
+CONF_NAME = "name"
+
+DEFAULT_NAME = "Water"
+DEFAULT_TOPIC = "wmbusmeters/Wasser"
+
+STORE_KEY = "water_saver_state"
+STORE_VERSION = 1
